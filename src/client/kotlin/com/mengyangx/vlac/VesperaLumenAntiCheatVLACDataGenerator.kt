4594b8064c1136a327acdce1bcca6e9010dd2041
MEng_YangX <MEng_YangX@outlook.com>
2025-04-07 12:42:01 +0800

    创建模组

---
+++ src/client/kotlin/com/mengyangx/vlac/VesperaLumenAntiCheatVLACDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.mengyangx.vlac
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object VesperaLumenAntiCheatVLACDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
